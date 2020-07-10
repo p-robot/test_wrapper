8cf93879fa1d8e2c24e145cbc7c6958de3ae9fba
Will Probert <will@probert.co.nz>
2020-07-10 23:31:03 +0100

    Update print_numbers.py

--- print_numbers.py
+++ print_numbers.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 print([0, 7, 34, 12])
-np.random.randint(0, 10, 10))
+print(np.random.randint(0, 10, 10))
 
 plt.plot(np.random.randint(0, 10, 100), color = 'red')
 plt.savefig("test.png")
