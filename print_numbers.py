5b91d645bd27a5edae8a4d27dbb6a99be3af38e0
Will Probert <will@probert.co.nz>
2020-07-10 23:55:54 +0100

    Update print_numbers.py

--- print_numbers.py
+++ print_numbers.py
@@ -1,9 +1,10 @@
-import numpy as np
-from matplotlib import pyplot as plt
+import numpy as np, pandas as pd
+#from matplotlib import pyplot as plt
 
 print([0, 7, 34, 12])
 print(np.random.randint(0, 10, 10))
 
-plt.plot(np.random.randint(0, 10, 100), color = 'red')
-plt.savefig("test.png")
-plt.close()
+# plt.plot(np.random.randint(0, 10, 100), color = 'red')
+# plt.savefig("test.png")
+# plt.close()
+pd.DataFrame({"h": [0, 1, 5, 3], "value": [0.2, 0.1, 0.32, 0.11]}).to_csv('test.csv', index = False)
