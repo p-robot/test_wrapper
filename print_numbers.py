e9e1f4b9510901e09059ad7e6b589dad5a61292d
Will Probert <will@probert.co.nz>
2020-07-11 00:12:13 +0100

    Update print_numbers.py

--- print_numbers.py
+++ print_numbers.py
@@ -7,4 +7,8 @@ print(np.random.randint(0, 10, 10))
 # plt.plot(np.random.randint(0, 10, 100), color = 'red')
 # plt.savefig("test.png")
 # plt.close()
+
+import pathlib
+pathlib.Path("output_file").mkdir(parents = True, exist_ok = True)
+
 pd.DataFrame({"h": [0, 1, 5, 3], "value": [0.2, 0.1, 0.32, 0.11]}).to_csv('output_file/test.csv', index = False)
