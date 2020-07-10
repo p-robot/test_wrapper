4c2ba0a8691e0a421ef7294f9b6481b375db2a29
Will Probert <will@probert.co.nz>
2020-07-11 00:41:40 +0100

    Update print_numbers.py

--- print_numbers.py
+++ print_numbers.py
@@ -9,6 +9,10 @@ print(np.random.randint(0, 10, 10))
 # plt.close()
 
 import os
-os.makedirs("output_file", exist_ok = True)
+from os.path import join
+output_dir = "output_file"
+os.makedirs(output_dir, exist_ok = True)
 
-pd.DataFrame({"h": [0, 1, 5, 3], "value": [0.2, 0.1, 0.32, 0.11]}).to_csv('output_file/test.csv', index = False)
+pd.DataFrame({"h": [0, 1, 5, 3], "value": [0.2, 0.1, 0.32, 0.11]}).to_csv(join(output_dir, "test.csv"), index = False)
+
+pd.DataFrame({"h": [1, 2, 4, 4], "value": [0.2, 0.1, 0.32, 0.91]}).to_csv(join(output_dir, "test_again.csv"), index = False)
